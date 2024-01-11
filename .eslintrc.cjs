6c3c24ffe82a4643300e9fa792524aeeae434ef8
Dmitriy Palii <it.n00b.pel@gmail.com>
2024-01-11 13:45:27 +0200

    fix: добавит eslint-plugin-vue для стилизации кода RTI-2640 RTI-2643

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,20 @@
-{
-    "env": {
-    // где будет исполняться наш код
-    "browser: true,
-    "node": true,
-        // добавляет все глобальные переменные ECMAScript 2023 и автоматически
-        //  устанавливает ecmaVersionопция парсера 14 в parserOptions
-        "es2023": true
-},
-    // Пресет с настройками. prettier должен быть последним.
-    // Он удаляет некоторые правила eslint из-за которых могут возникать конфликты.
-    // ВНИМАНИЕ!!! для vue 3 оставить 'plugin:vue/vue3-recommended', для nuxt 3 - '@nuxt/eslint-config'
-    "extends": [
-    "airbnb-base" || "airbnb-typescript/base" // если есть ts,
-    "@vue/eslint-config-typescript", // если есть ts
-    "plugin:import/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:vue/vue3-recommended" || "@nuxt/eslint-config", // если есть ts
-    "prettier",
-],
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2023: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:import/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'prettier',
+        'plugin:vue/essential'
+    ],
     parserOptions: {
-    // Позволяет использовать import/export, вызывает ошибкит при использовании ts
-    "sourceType": "module",
-        "ecmaVersion": "latest",
-},
-    "rules": {}
-}
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+    },
+    rules: {},
+};
