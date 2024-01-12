297fa91f93971b5dbd594948398b158af757b1c1
Dmitriy Palii <it.n00b.pel@gmail.com>
2024-01-12 17:31:33 +0200

    feat: RTI-2640 RTI-2643 создаст универсальный инпут https://track.ratio.digital/youtrack/issue/RTI-2640

---
+++ components/shared/input/ui/UniversaInput.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  enterHandler: {
+    type: Boolean,
+    default: false,
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+});
+const inputValue = ref(props.value);
+
+const emit = defineEmits(["updateInputValue", "enterTap"]);
+
+function onInput(event) {
+  inputValue.value = event.target.value;
+  emit("updateInputValue", inputValue.value);
+}
+
+const tapEnter = () => {
+  if (props.enterHandler) {
+    emit("enterTap");
+  }
+};
+
+</script>
+
+<template>
+  <input
+    :value="props.value"
+    @input="onInput"
+    @keyup.enter="tapEnter"
+  >
+</template>
+
+<style scoped lang="scss"></style>
